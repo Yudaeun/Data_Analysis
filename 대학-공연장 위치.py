d2d34ab72d70ce52ee5c7bba02da56b2f4f9033c
YUDAEUN <udu5771@naver.com>
2022-06-16 23:05:45 +0900

    서울시 대학 위치, 공연장 및 미술관 위치 지도로 나타내기

--- 대학-공연장 위치.py
+++ 대학-공연장 위치.py
@@ -6,14 +6,18 @@ df2=pd.read_excel("서울시 문화공간 정보.xlsx")
 df2=df2.dropna()
 df2['X좌표']=df2['X좌표'].astype(float)
 df2['Y좌표']=df2['Y좌표'].astype(float)
-df2[df2['주제분류'].str.contains('공연장',na=False)]
+
 seoul_map=folium.Map(location=[37.55,126.98],zoom_start=12)
 
 for _, row in df.iterrows():
   Marker(location=[row['위도'],row['경도']],popup=row['index'],
          icon=folium.Icon(color='blue')).add_to(seoul_map)
 
-for _, row in df2.iterrows():
-  Marker(location=[row['X좌표'],row['Y좌표']],popup=row['문화시설명'],icon=folium.Icon(color='pink')).add_to(seoul_map)
+for a, row in df2.iterrows():
+  if df2['주제분류'][a]=='공연장':
+    Marker(location=[row['X좌표'],row['Y좌표']],popup=row['문화시설명'],icon=folium.Icon(icon='star',color='pink')).add_to(seoul_map)
+  elif df2['주제분류'][a]=='미술관':
+    Marker(location=[row['X좌표'],row['Y좌표']],popup=row['문화시설명'],icon=folium.Icon(icon='star',color='darkblue')).add_to(seoul_map)
+
 
 seoul_map.save('seoul_universities.html')
